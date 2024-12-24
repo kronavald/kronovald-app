71e5253e90c4d74f66bf4cdb830de110350ba4ca
Tomoe <4.artem.0@gmail.com>
2024-12-25 00:23:34 +0300

    Formatting workspace files

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-    return <h1 class='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Hello, world!</h1>;
+    return (
+        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Hello, world!
+        </h1>
+    )
 }
 
-export default App;
+export default App
