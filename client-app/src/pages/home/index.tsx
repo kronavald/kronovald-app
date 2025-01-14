d4d7ccb83df361a7c130c94affed64d25791bb80
fruitbang <fruitbang91@gmail.com>
2025-01-14 22:21:27 +0300

    added get and patch file with some react-query magic

--- client-app/src/pages/home/index.tsx
+++ client-app/src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from "react-i18next"
-import TestComponent from "./TestComponent"
 
-import { useCallback, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Button } from "@/components/ui/button"
 import Editor from "@/components/editor"
 import { Editor as TipTapEditor } from "reactjs-tiptap-editor"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { defaultQueryFn } from "@/api"
+import { Loader2 } from "lucide-react"
 
 function debounce(func: any, wait: number) {
     let timeout: NodeJS.Timeout
@@ -15,9 +17,39 @@ function debounce(func: any, wait: number) {
     }
 }
 
+const patchFile = (content: string) =>
+    fetch("/api/text-documents", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ blob: content }),
+    })
+
 export default function Home() {
+    const queryClient = useQueryClient()
     const { t } = useTranslation("translation")
-    const [content, setContent] = useState("default")
+
+    const [content, setContent] = useState("")
+
+    const { data, isSuccess } = useQuery({
+        queryFn: () => defaultQueryFn({ queryKey: ["text-documents"] }),
+        queryKey: ["file"],
+    })
+
+    const {
+        mutate: mutateFile,
+        isPending,
+        isSuccess: isSuccessSave,
+    } = useMutation({
+        mutationFn: () => patchFile(content),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["file"] })
+        },
+        onError: () => {
+            throw new Error("Error saving file")
+        },
+    })
 
     const editorRef = useRef<{ editor: TipTapEditor } | null>(null)
 
@@ -30,28 +62,35 @@ export default function Home() {
         [],
     )
 
-    const handleSave = async () => {
-        const response = await fetch("/api/text-documents", {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ blob: content }),
-        })
-        console.log(response)
-    }
+    useEffect(() => console.log(content), [content])
+
+    const handleSave = async () => mutateFile()
 
     return (
         <div className="flex min-h-screen bg-gradient-to-b from-pink-700 to-purple-700">
             <section className="w-full py-32 md:py-48 flex flex-col items-center justify-center">
                 <h1 className="text-black">{t("title")}</h1>
-                <TestComponent />
-                <Editor
-                    onChange={handleChange}
-                    content={content}
-                    ref={editorRef}
-                />
-                <Button onClick={handleSave}>Save</Button>
+                {isSuccess && (
+                    <Editor
+                        onChange={handleChange}
+                        content={content ? content : data?.blob}
+                        ref={editorRef}
+                    />
+                )}
+                <Button onClick={handleSave} disabled={isPending}>
+                    {isPending ? <Loader2 /> : "Save"}
+                </Button>
+                {isSuccess && !isSuccessSave && (
+                    <div>
+                        Initial data from backend: {JSON.stringify(data.blob)}
+                    </div>
+                )}
+                {isSuccessSave && isSuccess && (
+                    <div>
+                        Successfully saved to backed data:{" "}
+                        {JSON.stringify(data.blob)}
+                    </div>
+                )}
             </section>
         </div>
     )
