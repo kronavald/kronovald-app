a5332a570ec61c942ed8dffac31a4a4210bdf132
fruitbang <fruitbang91@gmail.com>
2025-01-31 06:18:48 +0300

    added dark mode

--- client-app/src/components/app-sidebar.tsx
+++ client-app/src/components/app-sidebar.tsx
@@ -1,173 +1,65 @@
 import * as React from "react"
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react"
+import { Github, SquareTerminal } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarRail,
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenuSkeleton,
+    SidebarRail,
+    useSidebar,
 } from "@/components/ui/sidebar"
-
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
+import { LanguageSelector } from "./language-selector"
+import { Button } from "./ui/button"
+import { useTranslation } from "react-i18next"
+import { Link } from "react-router"
+import { Separator } from "@radix-ui/react-separator"
+import { ModeToggle } from "./mode-toggle"
+// import { useFilesListQuery } from "@/api/text-documents/queries"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
-  )
+    const { t } = useTranslation()
+    const { open } = useSidebar()
+
+    // const { data: files, isLoading } = useFilesListQuery()
+
+    // const navFilesItems = files
+    //     ? [
+    //           ...files.map((file) => ({
+    //               title: String(file.id),
+    //               url: `/:${file.id}`,
+    //           })),
+    //           { title: "New", url: ":new" },
+    //       ]
+    //     : [{ title: "New", url: ":new" }]
+
+    // const navFilesData = [{ title: "Files", url: "#", icon: SquareTerminal, isActive: true, items: navFilesItems }]
+
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <h1 className="font-bold center text-center">
+                    <Link to="/about" className="hover:text-blue-500 transition-colors duration-300">
+                        {open ? t("slogan") : null}{" "}
+                    </Link>
+                </h1>
+            </SidebarHeader>
+            <Separator orientation="horizontal" className="my-1" />
+            {/* <SidebarContent>{isLoading ? <SidebarMenuSkeleton /> : <NavMain items={navFilesData} />}</SidebarContent> */}
+            <SidebarFooter>
+                <div className="flex justify-between gap-4">
+                    <Button size={"icon"} className="rounded-full">
+                        <a href="https://github.com/kronavald/kronovald" target="_blank" rel="noreferrer">
+                            <Github />
+                        </a>
+                    </Button>
+                    <ModeToggle />
+                    <LanguageSelector />
+                </div>
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    )
 }
