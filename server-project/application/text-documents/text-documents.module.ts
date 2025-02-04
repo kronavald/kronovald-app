6c6db396952b2a94f4cf636525ca6ad13d0abd3b
Tomoe <4.artem.0@gmail.com>
2025-02-04 20:19:30 +0300

    rename application folders

--- server-project/application/text-documents/text-documents.module.ts
+++ server-project/application/text-documents/text-documents.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common"
 import { TextDocumentsService } from "./text-documents.service"
 import { TextDocumentsController } from "./text-documents.controller"
-import { PrismaService } from "src/prisma.service"
+import { PrismaService } from "application/prisma.service"
 
 @Module({
     controllers: [TextDocumentsController],
--- server-project/application/text-documents/text-documents.service.ts
+++ server-project/application/text-documents/text-documents.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common"
-import { PrismaService } from "src/prisma.service"
+import { PrismaService } from "application/prisma.service"
 import { Prisma } from "@prisma/client"
 
 @Injectable()
