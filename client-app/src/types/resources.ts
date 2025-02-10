834295c2d1ac4ee6db8730eaf8c7be47f0165530
Tomoe <4.artem.0@gmail.com>
2025-02-10 21:18:27 +0300

    fix some errors

--- client-app/src/types/resources.ts
+++ client-app/src/types/resources.ts
@@ -1,4 +1,6 @@
+/// @ts-expect-error Sanya, pochini facking i18n
 import translation from "../i18n/locales/en/translation.json"
+/// @ts-expect-error Sanya, pochini facking i18n
 import notfound from "../i18n/locales/en/notfound.json"
 
 const resources = {
